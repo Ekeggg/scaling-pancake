85e1032134d3c2b241329b34db6ea5bc35bc686f
Ekeggg <boombambopbadabopboomppow@gmail.com>
2026-04-28 14:47:22 +0800

    put baseURL in .env

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,19 @@
-import Counter from "./components/counter.jsx"
-import Navbar from "./components/navbar.jsx"
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const App = () => {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </div>
+    );
+};
 
-    return(
-        <>
-            <Navbar/>
-            <Counter/>
-            <Counter/>
-        </>
-    )
-}
-
-export default App
+export default App;
